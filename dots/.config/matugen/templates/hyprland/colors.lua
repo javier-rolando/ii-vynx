88cbd250059e2284c6f67faed6f3365a2d8e76f6
Javier Rolando <javier.rolando98@hotmail.com>
2026-05-15 12:40:36 -0300

    fix template hyprland matugen

--- dots/.config/matugen/templates/hyprland/colors.lua
+++ dots/.config/matugen/templates/hyprland/colors.lua
@@ -1,8 +1,8 @@
 hl.config({
     general = {
         col = {
-            active_border   = "rgba({{colors.outline_variant.default.hex_stripped}}77)",
-            inactive_border = "rgba({{colors.surface_container_low.default.hex_stripped}}33)",
+            active_border   = "rgba({{colors.primary.default.hex_stripped}}FF)",
+            inactive_border = "rgba({{colors.outline_variant.default.hex_stripped}}55)",
         },
     },
     misc = {
